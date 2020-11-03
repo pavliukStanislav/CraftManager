fd3c733afa0eb90b5a299915c2ea62d31927aca9
pavliukStanislav <pavliuk.stanislav@gmail.com>
2020-11-03 18:43:51 +0200

    environment variables

--- WoWAlchemistCalculator/src/environments/environment.prod.ts
+++ WoWAlchemistCalculator/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDFU-3kexobgit20BcTx-dhtlNdT1Nt06U",
+    authDomain: "wowalchemistcalculator.firebaseapp.com",
+    databaseURL: "https://wowalchemistcalculator.firebaseio.com",
+    projectId: "wowalchemistcalculator",
+    storageBucket: "wowalchemistcalculator.appspot.com",
+    messagingSenderId: "927015810163",
+    appId: "1:927015810163:web:b1300ad82cadc3e8c76606",
+    measurementId: "G-L2XXDY7JJP"
+  }  
 };
