93c0bc8f07b53bad6923f97d3755e5ceb2a97fbc
pavliukStanislav <pavliuk.stanislav@gmail.com>
2020-12-06 14:09:34 +0200

    prod data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDFU-3kexobgit20BcTx-dhtlNdT1Nt06U",
-    authDomain: "wowalchemistcalculator.firebaseapp.com",
-    databaseURL: "https://wowalchemistcalculator.firebaseio.com",
-    projectId: "wowalchemistcalculator",
-    storageBucket: "wowalchemistcalculator.appspot.com",
-    messagingSenderId: "927015810163",
-    appId: "1:927015810163:web:b1300ad82cadc3e8c76606",
-    measurementId: "G-L2XXDY7JJP"
+    apiKey: "AIzaSyBbQJORP6WMJD9pm1qRjoPDY-8LYohkqf4",
+    authDomain: "craftmanager-de7ad.firebaseapp.com",
+    projectId: "craftmanager-de7ad",
+    storageBucket: "craftmanager-de7ad.appspot.com",
+    messagingSenderId: "517830868195",
+    appId: "1:517830868195:web:b09bf3122e2ef5428f19d6",
+    measurementId: "G-BPW4GKYV01"
   }  
 };
